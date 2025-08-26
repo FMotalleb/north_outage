fb09bc6e564ff60bfb7e791d70f78fcd1bc7c465
Motalleb Fallahnezhad <fmotalleb@dornica.net>
2025-08-26 15:52:29 +0330

    fix: collector hash unique constraints

--- service/service.go
+++ service/service.go
@@ -2,9 +2,11 @@ package service
 
 import (
 	"context"
+	"slices"
 
 	"github.com/fmotalleb/go-tools/log"
 	"go.uber.org/zap"
+	"gorm.io/gorm"
 
 	"github.com/fmotalleb/north_outage/collector"
 	"github.com/fmotalleb/north_outage/config"
@@ -27,12 +29,33 @@ func Serve(ctx context.Context) error {
 		return err
 	}
 	l.Info("config initialized", zap.Any("cfg", cfg))
+	collectCycle(ctx, db)
+	return nil
+}
+
+func collectCycle(ctx context.Context, db *gorm.DB) error {
 	var data []models.Event
+	var err error
 	if data, err = collector.Collect(ctx); err != nil {
 		return err
 	}
-	tx := db.Create(data)
-	err = tx.Apply(db.Config)
-	println(err)
+
+	events := db.Table("events")
+	var oldHash []string
+	events.Select("hash").Find(&oldHash)
+	err = db.Transaction(
+		func(tx *gorm.DB) error {
+			for _, ev := range data {
+				if slices.Contains(oldHash, ev.Hash) {
+					continue
+				}
+				tx.Create(&ev)
+			}
+			return nil
+		},
+	)
+	if err != nil {
+		return err
+	}
 	return nil
 }
